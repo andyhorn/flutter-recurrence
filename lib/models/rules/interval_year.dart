782db837d313482968f1ef444673a7e3db895bc9
Andy <andyjhorn@gmail.com>
2021-06-13 12:24:00 -0500

    added value check to IntervalYear

--- lib/models/rules/interval_year.dart
+++ lib/models/rules/interval_year.dart
@@ -3,8 +3,15 @@ part of '../recurrence.dart';
 /// A recurrence rule that occurs every _n_ years after
 /// the beginning [DateTime].
 class IntervalYear extends Recurrence {
+  /// Create an [IntervalYear] instance.
+  ///
+  /// Throws an [ArgumentError] if [interval] is less than one.
   IntervalYear(int interval, DateTime startsOn)
-      : super([interval], startsOn, RecurrenceType.IntervalYear);
+      : super([interval], startsOn, RecurrenceType.IntervalYear) {
+    if (interval < 1) {
+      throw ArgumentError.value(interval, 'Must be one or greater.');
+    }
+  }
 
   /// Gets the interval on which this rule occurs.
   int get interval => _values.first;
